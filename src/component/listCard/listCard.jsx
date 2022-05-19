d86809bff94aa8feb836ea0f330200cb1b456935
mansii-shah <mansishah21179@gmail.com>
2022-05-19 23:30:34 +0530

    Update

--- src/component/listCard/listCard.jsx
+++ src/component/listCard/listCard.jsx
@@ -31,43 +31,114 @@ return(
 
 
 <Grid container align="center" direction="row">
-    <Grid item sm={12} >
-    <Stack align="center" direction="row"  spacing={8} style={listStyle}>
+    <Grid item sm={3} >
+    <Stack align="center" direction="column"  spacing={3} style={listStyle}>
     <Typography variant="h6"  >
     Development
     </Typography>
-    <Typography variant="h6">
-    Business
+    <Typography variant="h8" color="purple" >
+    Python
     </Typography>
-    <Typography variant="h6">
-    IT and Software
+    <Typography variant="h11"  >
+    34,887,515 students
     </Typography>
-    <Typography variant="h6">
-    Design
+    <Typography variant="h8" color="purple" >
+    Web Development
     </Typography>
+    <Typography variant="h11"  >
+    34,887,515 students
+    </Typography>
+    <Typography variant="h8" color="purple" >
+    Machine Learning
+    </Typography>
+    <Typography variant="h11"  >
+    34,887,515 students
+    </Typography>
+
+
     </Stack>
     </Grid>
-    </Grid>
-    
-<Grid container align="center" direction="column">
-    <Grid item sm={12}>
-    <Stack align="center" direction="column"  spacing={4} style={columnStyle}>
+    <Grid item sm={3} >
+    <Stack align="center" direction="column"  spacing={3} style={listStyle}>
     <Typography variant="h6"  >
-    Development
-    </Typography>
-    <Typography variant="h6">
     Business
     </Typography>
-    <Typography variant="h6">
-    IT and Software
+    <Typography variant="h8" color="purple" >
+    Financial Analysis
     </Typography>
-    <Typography variant="h6">
-    Design
+    <Typography variant="h11"  >
+    34,887,515 students
+    </Typography>
+    <Typography variant="h8" color="purple" >
+    SQL
+    </Typography>
+    <Typography variant="h11"  >
+    34,887,515 students
+    </Typography>
+    <Typography variant="h8" color="purple" >
+      PMP
+    </Typography>
+    <Typography variant="h11"  >
+    34,887,515 students
     </Typography>
     </Stack>
     </Grid>
+
+    <Grid item sm={3} >
+    <Stack align="center" direction="column"  spacing={3} style={listStyle}>
+    <Typography variant="h6"  >
+    IT and Software
+    </Typography>
+    <Typography variant="h8" color="purple" >
+    AWS
+    </Typography>
+    <Typography variant="h11"  >
+    34,887,515 students
+    </Typography>
+    <Typography variant="h8" color="purple" >
+    EITHICAL HACKING
+    </Typography>
+    <Typography variant="h11"  >
+    34,887,515 students
+    </Typography>
+    <Typography variant="h8" color="purple" >
+      CYBER_SECURITY
+    </Typography>
+    <Typography variant="h11"  >
+    34,887,515 students
+    </Typography>
+    </Stack>
     </Grid>
-    </Box>
+
+
+    <Grid item sm={3} >
+    <Stack align="center" direction="column"  spacing={3} style={listStyle}>
+    <Typography variant="h6"  >
+    Design
+    </Typography>
+    <Typography variant="h8" color="purple" >
+      PhotoShop
+    </Typography>
+    <Typography variant="h11"  >
+    34,887,515 students
+    </Typography>
+    <Typography variant="h8" color="purple" >
+    Graphics Design
+    </Typography>
+    <Typography variant="h11"  >
+    34,887,515 students
+    </Typography>
+    <Typography variant="h8" color="purple" >
+      Drawing
+    </Typography>
+    <Typography variant="h11"  >
+    34,887,515 students
+    </Typography>
+    </Stack>
+    </Grid>
+
+       </Grid>
+     </Box>
 )
 }
 export default ListCard;
